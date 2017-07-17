78497a4eadd989ae1c99c414b9da9b6aea26b992
TamyhB <blenda.tamy@gmail.com>
2017-07-17 20:13:11 -0300

    ta quase?

--- GeneticAlgoritm.py
+++ GeneticAlgoritm.py
@@ -2,7 +2,7 @@ import numpy as np
 from random import randint
 import random
 
-mutation = 0.01
+mutacao = 0.1
 probabilidade = None
 queens = 8
 
@@ -29,7 +29,7 @@ def create_populacao(x):
     for i in range(0, x):
         x = GeneticAlgoritm()
         x.tabuleiro = criar_cromossomo(8)
-        x.avaliacao = contar_confrontos_rainha(x)
+        x.avaliacao = avaliar_confrontos_rainhas(x)
         population.append(x)
     return population
 
@@ -55,19 +55,24 @@ def algGenetic(population):
             x = selecao_aleatoria_ponderada(population)
             y = selecao_aleatoria_ponderada(population)
             child, child2 = reproduction(x,y)
-            child_F = None
+            best_child = None
             if(child.avaliacao <= child2.avaliacao):
-                child_F = child
+                best_child = child
             else:
-                child_F = child2
-            #probabilidade = random.random()
-            #if mutation >= probabilidade:
-             #   child = mutation(child_F)
-              #  fnPopulation.append(child_F)
-            #else:
-            nova_populacao.append(child_F)
+                best_child = child2
+            probabilidade = random.random()
+            print probabilidade
+            if probabilidade <= mutacao:
+                best_child = mutation(best_child)
+                best_child.avaliacao = avaliar_confrontos_rainhas(best_child)
+                nova_populacao.append(best_child)
+            else:
+                nova_populacao.append(best_child)
         population = nova_populacao
         individuo = minimo_vetor(nova_populacao)
+        print "LEN POPULACAO " + str(len(population))
+        print "MINIMO ATUAL " + str(individuo.avaliacao)
+        print "MINIMO ATUAL " + str(individuo.tabuleiro)
         if individuo.avaliacao == 0:
             break
     return individuo
@@ -95,31 +100,37 @@ def selecao_aleatoria_ponderada(population):
         else:
             faixa4.append(rainha)
 
-    dado_100_faces = randint(1, 100)
+    while 1:
+        dado_100_faces = randint(1, 100)
 
-    if dado_100_faces <= 5:
-        tamanho = len(faixa4) -1
-        indice = randint(0, tamanho)
-        return faixa4[indice]
-    elif dado_100_faces <= 15 and dado_100_faces > 5:
-        tamanho = len(faixa3) - 1
-        indice = randint(0, tamanho)
-        return faixa3[indice]
-    elif dado_100_faces <= 45 and dado_100_faces > 15:
-        tamanho = len(faixa2) - 1
-        indice = randint(0,tamanho)
-        return faixa2[indice]
-    elif 100 >= dado_100_faces > 45:
-        tamanho = len(faixa1) - 1
-        indice = randint(0, tamanho)
-        return faixa1[indice]
+        if dado_100_faces <= 5:
+            if len(faixa4) > 0:
+                tamanho = len(faixa4) -1
+                indice = randint(0, tamanho)
+                return faixa4[indice]
+        elif dado_100_faces <= 15 and dado_100_faces > 5:
+            if len(faixa3) > 0:
+                tamanho = len(faixa3) - 1
+                indice = randint(0, tamanho)
+                return faixa3[indice]
+        elif dado_100_faces <= 45 and dado_100_faces > 15:
+            if len(faixa2) > 0:
+                tamanho = len(faixa2) - 1
+                indice = randint(0,tamanho)
+                return faixa2[indice]
+        elif 100 >= dado_100_faces > 45:
+            if len(faixa1) > 0:
+                tamanho = len(faixa1) - 1
+                indice = randint(0, tamanho)
+                return faixa1[indice]
+
 
 
 def reproduction(crossX, crossY):
     x = GeneticAlgoritm()
     y = GeneticAlgoritm()
-    print crossX
-    print crossY
+    #print crossX.tabuleiro
+    #print crossY.tabuleiro
     tuplaSize = crossY.tabuleiro.shape
     n = tuplaSize[0] - 1
     s = np.random.randint(n, size=1)
@@ -133,19 +144,19 @@ def reproduction(crossX, crossY):
     y.tabuleiro = np.array(aux_y)
     #y.tabuleiro.extend(crossX.tabuleiro[0:(len(crossX.tabuleiro) - 1) - n])
     #y.tabuleiro.extend(crossY.tabuleiro[abs((len(crossX.tabuleiro) - 1) - n):])
-    x.avaliacao = contar_confrontos_rainha(x)
-    y.avaliacao = contar_confrontos_rainha(y)
+    x.avaliacao = avaliar_confrontos_rainhas(x)
+    y.avaliacao = avaliar_confrontos_rainhas(y)
     return x, y
 
 
-# def mutation(filho):
-#     print "Mutation!!!"
-#     i = randint(0,7)
-#     x = randint(0,7)
-#     filho = filho.tabuleiro[i] = x
-#     return filho
+def mutation(filho):
+    print "Mutation!!!"
+    i = randint(0,7)
+    x = randint(0,7)
+    filho.tabuleiro[i] = x
+    return filho
 
-def contar_confrontos_rainha(candidate):
+def avaliar_confrontos_rainhas(candidate):
     confrontos = 0
     col_confrontos = abs(len(candidate.tabuleiro) - len(np.unique(candidate.tabuleiro)))
     confrontos = confrontos + col_confrontos
@@ -159,7 +170,7 @@ def contar_confrontos_rainha(candidate):
 
     return  confrontos
 
-nova_populacao = create_populacao(5)
+nova_populacao = create_populacao(1000)
 result = algGenetic(nova_populacao)
 print result.tabuleiro
 print result.avaliacao
